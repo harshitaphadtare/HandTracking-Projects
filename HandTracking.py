69389ff0a9fafba95942da3ec9c17df2d521689b
Harshita Phadtare <hphadtare2023@gmail.com>
2023-10-18 19:41:15 +0530

    handTracking

---
+++ HandTracking.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+cap = cv.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success,img = cap.read()
+    imgRGB = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    
+    if results.multi_hand_landmarks:
+        for handLandmarks in results.multi_hand_landmarks:
+            for id,lm in enumerate(handLandmarks.landmark):
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w),int(lm.y*h)
+                # if id == 4:
+                #     cv.circle(img,(cx,cy),15,(255,0,255),cv.FILLED)
+
+
+            mpDraw.draw_landmarks(img,handLandmarks,mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv.putText(img,str(int(fps)),(10,70),cv.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+
+    cv.imshow("image",img)
+    cv.waitKey(1)
